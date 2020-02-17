358271100a7e960631d80917f07d3246a05cdc7c
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-02-16 23:18:38 -0300

    verificando se o usuario exixte

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,10 +1,17 @@
 const Spot = require('../models/Spot')
+const User = require('../models/User')
 
 module.exports = {
     async store(req,res){
         const {filename} = req.file
         const {company, techs, price} = req.body
         const {user_id} = req.headers
+
+        const user = await User.findById(user_id)
+
+        if(!user){
+            return res.status(400)
+        }
 
         const spot = await Spot.create({
             user: user_id,
