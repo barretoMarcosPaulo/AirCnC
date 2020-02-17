f51e5716d06b0481d999e81d3e416e0f788f4372
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-02-16 22:41:39 -0300

    criado model e controller para a Session

---
+++ backend/src/controllers/SessionController.js
@@ -0,0 +1,12 @@
+const User = require('../models/User')
+
+module.exports = {
+   async store(req, res){
+       const {email} = req.body
+
+       const user = await User.create({ email })
+     
+       return res.json(user)
+    
+    }
+}
