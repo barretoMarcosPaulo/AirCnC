284e8ea541be7abcf0bbc3bb4d6ccf53c0e3095b
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-02-16 23:54:00 -0300

    filtrando spots do usuario logado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,11 +4,19 @@ const uploadConfig = require('./config/upload')
 
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
+const DashboardController = require('./controllers/DashboardController')
+
 
 const routes = express.Router()
 const upload = multer(uploadConfig)
 
 routes.post('/sessions', SessionController.store)
+
 routes.post('/spots',upload.single('thumbnail'),SpotController.store)
+routes.get('/spots',SpotController.index)
+
+routes.get('/dashboard',DashboardController.show)
+
+
 
 module.exports = routes
