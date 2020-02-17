02a9f9581503e033316d03f5d3c4f1a2f595bcce
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-02-16 23:14:15 -0300

    controller para o cadastro de spots

---
+++ backend/src/controllers/SpotController.js
@@ -0,0 +1,18 @@
+const Spot = require('../models/Spot')
+
+module.exports = {
+    async store(req,res){
+        const {filename} = req.file
+        const {company, techs, price} = req.body
+        const {user_id} = req.headers
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            price,
+            techs: techs.split(',').map(tech=> tech.trim())
+        })
+        return res.json(spot)
+    }
+}
