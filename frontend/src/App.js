0f7672f0cf6d71a8bfabf57e1d8bf52a90cf2461
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-02-19 15:42:00 -0300

    criando pagina de login com o reactjs

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+import logo from './assets/logo.svg'
+
+function App() {
+  return (
+    <div className="container">
+        <img src={logo} alt="LOGO"/>
+
+        <div className="content">
+            <p>
+              Ofereça <strong>spots </strong> para programadores e 
+              econtre <strong>talentos</strong> para sua empresa.
+            </p>
+
+            <form>
+     
+              <label htmlFor="email">E-MAIL*</label>
+     
+              <input 
+                type="email" 
+                id="email"
+                placeholder="Seu melhor email" 
+              />
+
+              <button className="btn" type="submit">Entrar</button>
+              
+            </form>
+        </div>
+    </div>
+   
+  );
+}
+
+export default App;
