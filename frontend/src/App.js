7ba11a9a635cb04fa39dba464d997f9eb2d0b524
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-02-22 17:48:59 -0300

    adicionada rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,20 @@
-import React,{ useState } from 'react';
+import React from 'react';
 import api from "./services/api"
 import './App.css';
 import logo from './assets/logo.svg'
 
+import Routes from './routes'
+import { Route } from 'react-router-dom';
+
 function App() {
 
-  const [email, setEmail] = useState('')
-
-   async function handleSubmit(event) {
-      event.preventDefault()
-      const response = await api.post('/sessions',{email})
-      const {_id } = response.data
-      localStorage.setItem('user',_id)
-  }
 
   return (
     <div className="container">
         <img src={logo} alt="LOGO"/>
 
         <div className="content">
-            <p>
-              Ofereça <strong>spots </strong> para programadores e 
-              econtre <strong>talentos</strong> para sua empresa.
-            </p>
-
-          <form onSubmit={handleSubmit}>
-     
-              <label htmlFor="email">E-MAIL*</label>
-     
-              <input 
-                type="email" 
-                id="email"
-                placeholder="Seu melhor email" 
-                value = {email}
-                onChange={event => setEmail(event.target.value)}
-              />
-
-              <button className="btn" type="submit">Entrar</button>
-
-            </form>
+            <Routes/>
         </div>
     </div>
    
