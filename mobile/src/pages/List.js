413f82499bccfeff50fd359c09bed0489ce11477
barretoMarcosPaulo <marcosbarretopaulo@gmail.com>
2020-04-10 21:39:42 -0300

    criado arquivo para pagina de List

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+    StyleSheet,
+    KeyboardAvoidingView,
+    View,
+    Text,
+    TextInput,
+    TouchableOpacity
+} from 'react-native'
+
+export default function List() {
+
+    return <View />
+}
